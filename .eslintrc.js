26bdbe962fa32b8775d928cf551af98576dc64f2
Zanuda-Dzmitry <119176161+Zanuda-Dzmitry@users.noreply.github.com>
2023-10-04 15:25:57 +0300

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+var thing = new Thing(); // eslint-disable-line no-use-before-define
+module.exports = {
+  root: true,
+  extends: ["airbnb-base"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  env: {
+    browser: true,
+  },
+  rules: {
+    "no-alert": 0,
+    "no-param-reassign": [2, { props: false }],
+    "no-plusplus": 0,
+    "no-iterator": 0,
+    "no-restricted-syntax": [2, "WithStatement"],
+    "func-style": 0,
+  },
+};
